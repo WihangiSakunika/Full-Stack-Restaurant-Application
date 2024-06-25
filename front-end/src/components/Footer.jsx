191c2b6123e49310ce2ad4d77f1bd28964f4e2b3
WihangiSakunika <lhewagewihangi@gmail.com>
2024-06-25 13:00:09 +0530

    mern

---
+++ front-end/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">Cook with Zhan Ge Ge</div>
+          <div className="right">
+            <p>Piliyandala, Sri Lanka</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By WihangiSakunika</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By WihangiSakunika.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ front-end/src/components/Qualities.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import data from '../restApi.json'; // Ensure the import is correct
+
+const Qualities = () => {
+  return (
+    <>
+      <section className='qualities' id='qualities'>
+        <div className="container">
+          {
+            data.data[0].ourQualities.map(element => ( // Access the nested data correctly
+              <div className='card' key={element.id}>
+                <img src={element.image} alt={element.title} />
+                <p className='title'>{element.title}</p>
+                <p className='description'>{element.description}</p>
+              </div>
+            ))
+          }
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Qualities;
